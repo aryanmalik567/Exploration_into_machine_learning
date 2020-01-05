62fea9c381ccaebabe76309c0562cd618101589f
Aryan <aryanmalik567@gmail.com>
2020-01-05 20:16:30 +0000

    Optimization

--- NEA_code/NEA_code.py
+++ NEA_code/NEA_code.py
@@ -7,11 +7,7 @@ import quandl
 
 stockToRequest = input("Enter stock symbol of stock you would like to fetch: ")
 
-quandl.ApiConfig.api_key = "BzmAGpzByrxtohyARK2B"
-stock = quandl.get(f"EOD/{stockToRequest}")
-stock = pd.DataFrame(stock)
+stock = quandl.get(f"EOD/{stockToRequest}", api_key="BzmAGpzByrxtohyARK2B")
+stock = stock[["Close"]]
 
-neededColumns = ['Adj_Close']  # Just uni-variate to begin with
-stock = stock[neededColumns]
-
-print(stock.head())
+print(stock.shape)
