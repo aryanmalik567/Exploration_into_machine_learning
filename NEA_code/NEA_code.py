774f2beabdd4b7fe38f943e1bc0873d43608134a
Aryan <aryanmalik567@gmail.com>
2020-01-27 23:11:04 +0000

    Starting sequencing

--- NEA_code/NEA_code.py
+++ NEA_code/NEA_code.py
@@ -12,21 +12,47 @@ quandl.ApiConfig.api_key = "BzmAGpzByrxtohyARK2B"
 stock = quandl.get(f"EOD/AAPL")
 stock = pd.DataFrame(stock)
 
-neededColumns = ['Adj_Close', 'Adj_Volume']
+neededColumns = ['Adj_Close']  # Add in volume later
 stock = stock[neededColumns]
 
-# Creating 60% train split
+'''
+# Visualizing our data to begin with
+plt.plot(stock)
+plt.xlabel('Date')
+plt.ylabel('AdjClose')
+plt.show()
+'''
+
+# Creating 80% train split
 numRows = stock.shape[0]
-first60pct = round(0.6 * numRows)
+first80pct = round(0.8 * numRows)
 
-stockTrain = stock[:first60pct]
-stockTrainStd = stockTrain.mean()
-stockTrainMean = stockTrain.std()
+stockTrain = stock[:first80pct]
+stockTrainMean = stockTrain.mean()
+stockTrainStd = stockTrain.std()
 
-stockNormalized = (stockTrain - stockTrainMean) / stockTrainStd
+stockTrainNormalized = (stockTrain - stockTrainMean) / stockTrainStd
 
-# print(stockNormalized.shape) gives (654, 2)
+# print(stockTrainNormalized.shape) gives (872, 1)
 
+# Creating 20% testing split
+last20pct = round(0.2 * numRows)
+
+stockTest = stock[first80pct:last20pct]
+stockTestMean = stockTest.mean()
+stockTestStd = stockTest.std()
+
+stockValNormalized = (stockTest - stockTestMean) / stockTestStd
+
+dataPoints = 300  # 300 time steps worth of training data for each sequence
+futurePeriodPrediction = 30  # Predicting 30 time steps into the future in each sequence
+
+x_train = []  # List containing several sequences each of time step length 300
+y_train = []  # List containing several sequences each of time step length 30
+
+for i in range(dataPoints, len(stockTrainNormalized)):
+    x_train.append(stockTrainNormalized[i - dataPoints:i])
+    y_train.append(stockTrainNormalized[i, 0])
 
 
 
