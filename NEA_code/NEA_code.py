02d0db22934c5f224e26f34dcb898927fd7f2996
Aryan <aryanmalik567@gmail.com>
2020-02-10 23:10:14 +0000

    Beginning training stage

--- NEA_code/NEA_code.py
+++ NEA_code/NEA_code.py
@@ -55,12 +55,33 @@ y_train = []  # List containing several sequences each of time step length 30, v
 for x in range(dataPoints, (len(stockTrainStandardized) - futurePeriodPrediction)):  # From 300 to 730
     x_train.append(stockTrainStandardized[x - dataPoints:x])  # Append 300 time steps from range 0 to 460
 
+# x_train = np.array(x_train)
+
 for y in range(dataPoints, (len(stockTrainStandardized) - futurePeriodPrediction)):  # From 30 to 760
     y_train.append(stockTrainStandardized[y:y + futurePeriodPrediction])  # Append 30 time steps from range 30 to 490
 
-x_test = stockTestNormalized[:dataPoints]
+x_test = stockTestNormalized[:dataPoints]  # First 300 time steps of test sequence
 y_test = stockTestNormalized[dataPoints:(dataPoints + futurePeriodPrediction)]
 
-BATCH_SIZE = 256
+BATCH_SIZE = 300
 BUFFER_SIZE = 10000
+
+'''
+train_data = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+train_data = train_data.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
+
+test_data = tf.data.Dataset.from_tensor_slices((x_test, y_test))
+test_data = test_data.batch(BATCH_SIZE).repeat()
+
+stockModel = tf.keras.models.Sequential()
+stockModel.add(tf.keras.layers.LSTM(32, return_sequences=True, input_shape=x_train.shape[-2:]))
+stockModel.add(tf.keras.layers.LSTM(16, activation='relu'))
+stockModel.add(tf.keras.layers.Dense(30))
+
+stockModel.compile(optimizer='adam', loss='mean_squared_error')
+stockModel.fit(x_train, y_train, epochs=100, batch_size=BATCH_SIZE)
+'''
+
+print(len(x_train))
+print(len(x_train[1][1]))
 
