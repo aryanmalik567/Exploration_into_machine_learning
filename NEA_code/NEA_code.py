ad3c17d76d496e7117fdbb6c91dbe75b6e9e14dd
Aryan <aryanmalik567@gmail.com>
2020-04-03 19:13:34 +0100

    Code tidying

--- NEA_code/NEA_code.py
+++ NEA_code/NEA_code.py
@@ -1,72 +1,72 @@
-import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import pandas as pd
 import quandl
-import tensorflow as tf
-import random
 from pandas.plotting import register_matplotlib_converters
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import tensorflow as tf
 register_matplotlib_converters()
-
-# stockToRequest = input("Enter stock symbol of stock you would like to fetch: ")
-
 quandl.ApiConfig.api_key = "BzmAGpzByrxtohyARK2B"
-# stock = quandl.get(f"EOD/{stockToRequest}")
-stock = quandl.get(f"EOD/MSFT")
-stock = pd.DataFrame(stock)
 
-neededColumns = ['Open', 'High', 'Low', 'Close', 'Volume']
-stock = stock[neededColumns]
 
-dataPoints = 300  # 300 time steps worth of training data for each sequence
-futurePeriodPrediction = 30  # Predicting 30 time steps into the future in each sequence
+modelTrial = 1  # First model
 
-# Visualizing our data to begin with
-'''
-plt.plot(stock['Close'])
-plt.xlabel('Date')
-plt.ylabel('Close')
-plt.show()
-'''
 
-# Creating train split
-numRows = stock.shape[0]
-trainSplit = numRows - futurePeriodPrediction - dataPoints
+def const_selector(model_number):
+    epochs_list = [10, 20, 30, 40]
+    epochSteps_list = [40, 30, 20, 10]
 
-stockTrain = stock[:trainSplit]
-stockTrainMean = stockTrain.mean()
-stockTrainStd = stockTrain.std()
+    return epochs_list[model_number], epochSteps_list[model_number]
 
-stockTrainStandardized = (stockTrain - stockTrainMean) / stockTrainStd
 
-stockTrainStandardized = np.array(stockTrainStandardized)
+class Constants:
+    def __init__(self, past_period, future_period, batch_size, buffer_size, epochs, epoch_steps, val_steps):
+        self.past_period = past_period
+        self.future_period = future_period
+        self.batch_size = batch_size
+        self.buffer_size = buffer_size
+        self.epochs = epochs
+        self.epoch_steps = epoch_steps
+        self.val_steps = val_steps
 
-# print(stockTrainStandardized.shape) gives (760, 5)
 
-# Creating testing split
-stockTest = stock[trainSplit:numRows]
-stockTestMean = stockTest.mean()
-stockTestStd = stockTest.std()
+consts = Constants(300, 30, 300, 10000, const_selector()[0], const_selector()[1], 10)
 
-stockTestStandardized = (stockTest - stockTestMean) / stockTestStd
 
-stockTestStandardized = np.array(stockTestStandardized)
+def quandl_request():
+    # stockToRequest = input("Enter stock symbol of stock you would like to fetch: ")
+    # stock = quandl.get(f"EOD/{stockToRequest}")
+    stock = quandl.get(f"EOD/MSFT")
+    stock = pd.DataFrame(stock)
+    needed_columns = ['Open', 'High', 'Low', 'Close', 'Volume']
+    stock = stock[needed_columns]
 
-# print(stockTestNormalized.shape) gives (330, 5)
-'''
-x_train = []  # List containing several sequences each of time step length 300, training data
-y_train = []  # List containing several sequences each of time step length 30, validation data
-'''
+    return stock
 
-'''
-for x in range(dataPoints, (len(stockTrainStandardized) - futurePeriodPrediction)):  # From 300 to 730
-    x_train.append(stockTrainStandardized[x - dataPoints:x])  # Append 300 time steps from range 0 to 460
 
-for y in range(dataPoints, (len(stockTrainStandardized) - futurePeriodPrediction)):  # From 300 to 730
-    y_train.append(stockTrainStandardized[y:y + futurePeriodPrediction])  # Append 30 time steps from range 300 to 730
-'''
+def data_split(stock):
+    # Creating training split
+    numRows = stock.shape[0]
+    trainSplit = numRows - consts.future_period - consts.past_period
+
+    stockTrain = stock[:trainSplit]
+    stockTrainMean = stockTrain.mean()
+    stockTrainStd = stockTrain.std()
+
+    stockTrainStandardized = (stockTrain - stockTrainMean) / stockTrainStd
+    stockTrainStandardized = np.array(stockTrainStandardized)
+
+    # Creating testing split
+    stockTest = stock[trainSplit:numRows]
+    stockTestMean = stockTest.mean()
+    stockTestStd = stockTest.std()
+
+    stockTestStandardized = (stockTest - stockTestMean) / stockTestStd
+    stockTestStandardized = np.array(stockTestStandardized)
+
+    return stockTrainStandardized, stockTestStandardized
 
 
 def sequencer(dataset, history_size, target_size):
@@ -85,27 +85,23 @@ def sequencer(dataset, history_size, target_size):
     return np.array(training_data), np.array(target_data)
 
 
-x_train, y_train = sequencer(stockTrainStandardized, dataPoints, futurePeriodPrediction)
+def x_y(stock_train, stock_test):
+    x_train, y_train = sequencer(stock_train, consts.past_period, consts.future_period)
 
-np.swapaxes(x_train, 0, 2)
-# np.swapaxes(y_train, 0, 2)
+    np.swapaxes(x_train, 0, 2)
 
-x_test = stockTestStandardized[:dataPoints]  # First 300 time steps of test sequence
+    x_test = stock_test[:consts.past_period]  # First 300 time steps of test sequence
 
-stockTestStandardized = stockTestStandardized[:, 4]
-y_test = stockTestStandardized[dataPoints:(dataPoints + futurePeriodPrediction)]
+    stock_test = stock_test[:, 4]
+    y_test = stock_test[consts.past_period:(consts.past_period + consts.future_period)]
 
-x_test = np.array(x_test)
-y_test = np.array(y_test)
+    x_test = np.array(x_test)
+    y_test = np.array(y_test)
 
-x_test = np.reshape(x_test, (1, 300, 5))
-y_test = np.reshape(y_test, (1, 30))
+    x_test = np.reshape(x_test, (1, 300, 5))
+    y_test = np.reshape(y_test, (1, 30))
 
-BATCH_SIZE = 300
-BUFFER_SIZE = 10000
-
-EpochSteps = 50
-EPOCHS = 20
+    return x_train, y_train, x_test, y_test
 
 
 def plot_output(history, actual_future, prediction):
@@ -121,25 +117,36 @@ def plot_output(history, actual_future, prediction):
     plt.show()
 
 
-train_data = tf.data.Dataset.from_tensor_slices((x_train, y_train))
-train_data = train_data.cache().batch(BATCH_SIZE).repeat()
+def train_model(x_train, y_train, x_test, y_test):
+    train_data = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+    train_data = train_data.cache().batch(consts.batch_size).repeat()
 
-test_data = tf.data.Dataset.from_tensor_slices((x_test, y_test))
-test_data = test_data.batch(BATCH_SIZE).repeat()
+    test_data = tf.data.Dataset.from_tensor_slices((x_test, y_test))
+    test_data = test_data.batch(consts.batch_size).repeat()
 
-stockModel = tf.keras.models.Sequential()
-stockModel.add(tf.keras.layers.LSTM(32, return_sequences=True, input_shape=x_train.shape[-2:]))
-stockModel.add(tf.keras.layers.LSTM(16, activation='relu'))
-stockModel.add(tf.keras.layers.Dense(30))
+    stockModel = tf.keras.models.Sequential()
+    stockModel.add(tf.keras.layers.LSTM(32, return_sequences=True, input_shape=x_train.shape[-2:]))
+    stockModel.add(tf.keras.layers.LSTM(16, activation='relu'))
+    stockModel.add(tf.keras.layers.Dense(30))
 
-# stockModel.compile(optimizer='adam', loss='mean_squared_error')
-stockModel.compile(optimizer=tf.keras.optimizers.RMSprop(clipvalue=1.0), loss='mae')
-stockModelHistory = stockModel.fit(train_data, epochs=EPOCHS, steps_per_epoch=EpochSteps, validation_data=test_data, validation_steps=10)
+    # stockModel.compile(optimizer='adam', loss='mean_squared_error')
+    stockModel.compile(optimizer=tf.keras.optimizers.RMSprop(clipvalue=1.0), loss='mae')
+    stockModelHistory = stockModel.fit(train_data, epochs=consts.epochs, steps_per_epoch=consts.epoch_steps, validation_data=test_data, validation_steps=consts.val_steps)
 
-# plot_output(x_test, y_test, stockModel.predict(x_test))
+    # plot_output(x_test, y_test, stockModel.predict(x_test))
 
-for x, y in test_data.take(1):
-    plot_output(x[0], y[0], stockModel.predict(x)[0])
+    for x, y in test_data.take(1):
+        plot_output(x[0], y[0], stockModel.predict(x)[0])
+
+
+def main():
+    stock = quandl_request()
+    train_test = data_split(stock)
+    xy_arrays = x_y(train_test[0], train_test[1])
+    train_model(xy_arrays[0], xy_arrays[1], xy_arrays[2], xy_arrays[3])
+
+
+main()
 
 
 
